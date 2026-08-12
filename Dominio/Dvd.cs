bffc5accf1772e1e4a221162d612fedfe51d32bc
cvcaju <cvcaju@gmail.com>
2026-08-11 21:10:45 -0300

    Feat: Criação do Construtor do ItemAcervo e criação da primeira herança

---
+++ Dominio/Dvd.cs
@@ -0,0 +1,11 @@
+namespace Biblioteca.Dominio;
+
+public class Dvd(string titulo, string autor) : ItemAcervo(titulo, autor)
+{
+   public override int PrazoDevolucao => 3;
+
+   public override decimal MultaDiariaAtrasado => 3m;
+
+}
+
+    
--- Dominio/ItemAcervo.cs
+++ Dominio/ItemAcervo.cs
@@ -2,18 +2,30 @@ namespace Biblioteca.Dominio;
 
 public abstract class ItemAcervo
 {
+    public ItemAcervo(string titulo, string autor)
+    {
+        if (string.IsNullOrWhiteSpace(titulo))
+
+        {
+            throw new ExcecaoDominio("O título é obrigatório");
+        }
+        Titulo = titulo;
+        Autor = autor;
+    }
+          
+    
     public string Titulo { get; set; } = string.Empty;
 
     public string Autor { get; set; } = string.Empty;
     public bool Disponibilidade { get; private set; } = true;
 
-    public abstract int PrazoDevolucao { get; set; }
-    
-    public abstract decimal MultaDiariaAtrasado { get; set; }
+    public abstract int PrazoDevolucao { get; }
+
+    public abstract decimal MultaDiariaAtrasado { get; }
 
     public decimal CalcularMulta(int diasAtrasados)
     {
-        return diasAtrasados >= 0 ? diasAtrasados * MultaDiariaAtrasado : 0;        
+        return diasAtrasados >= 0 ? diasAtrasados * MultaDiariaAtrasado : 0;
     }
 
 
