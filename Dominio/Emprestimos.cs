b4f1ef4cef76b2d99346bf6e1ea375aaec49d8fa
cvcaju <cvcaju@gmail.com>
2026-08-13 21:19:37 -0300

    feat: Registrando devolução do empréstimo e criando itens da biblioteca

--- Dominio/Emprestimos.cs
+++ Dominio/Emprestimos.cs
@@ -2,16 +2,33 @@ namespace Biblioteca.Dominio;
 
 public class Emprestimo
 {
-   public ItemAcervo Item { get; }
-   public DateTime DataEmprestimo { get; private set; } = DateTime.Today;
-   public DateTime PrazoLimite { get; }
+    public ItemAcervo Item { get; }
+    public DateTime DataEmprestimo { get; private set; } = DateTime.Today;
+    public DateTime PrazoLimite { get; }
 
-   public Emprestimo (ItemAcervo item)
+    public Emprestimo(ItemAcervo item)
     {
+        item.MarcarComoEmprestado();
         Item = item;
+        PrazoLimite = DataEmprestimo.AddDays(item.PrazoDevolucao);
     }
 
+    public decimal MultaAtual => Item.CalcularMulta();
 
-   
+    public int QtdDiasAtrasados
+    {
+        get
+        {
+            TimeSpan diasAtrasados = DateTime.Today - PrazoLimite;
+
+            return diasAtrasados.Days;
+        }
+    }
+
+    public void RegistrarDevolucao() 
+    {
+       
+         Item.MarcarComoDevolvido();
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Biblioteca.Dominio;
+
+ItemAcervo livro = new Livros("O Senhor dos Anéis", "J.R.R. Tolkien");
+
+ItemAcervo Margens = new Livros("Nas margens do rio Piedras eu sentei e chorei", "Paulo Colelho");
+
+ItemAcervo Principe = new Livros("O Pequeno Príncipe", "Antoine de Saint-Exupéry");
+
+ItemAcervo Memorias = new Livros("Memórias Póstumas de Brás Cubas", "Machado de Assis");
+
+ItemAcervo Estrela = new Livros("Mistério do Cinco Estrelas", "Marcos Reys");
+
+ItemAcervo Aventura = new Livros("Aventuras de Sherlock Holmes", "Arthur Conan Doyle");
+
+ItemAcervo Eletronica = new Revista("Eletrônica", "Senai");
+
+ItemAcervo Palavras = new Revista("Palavras Cruzadas", "Autor Desconhecido");
+
+ItemAcervo Raul = new Dvd("Rock Roll", "Raul Seixas");
+
+Emprestimo emprestimo = new Emprestimo(Principe);
+
+
+
+
+
+
+
