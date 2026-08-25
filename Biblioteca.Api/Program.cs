d273a5a92b6ca626a9d7939c9275a19b1045ad51
cvcaju <cvcaju@gmail.com>
2026-08-25 20:40:11 -0300

    feat: rotas de leitura de acervo e dos usuários

--- Biblioteca.Api/Program.cs
+++ Biblioteca.Api/Program.cs
@@ -5,12 +5,39 @@ using Biblioteca.Dominio;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Redirect("/itens"));
 
 var acervo = new Acervos();
 var cadastro = new Cadastro();
 Seed.Popular(acervo, cadastro);
 
+app.MapGet("/", () => Results.Redirect("/itens"));
+
 app.MapGet("/itens", () => acervo.Itens);
 
+app.MapGet("/itens/{id:int}", (int id) => 
+
+{
+    var item = acervo.BuscarPorId(id);
+
+    if( item is null)
+
+    {
+       return Results.NotFound(new { erro = $"o {id} do Item não foi encontrado!"});
+    }
+
+  return Results.Ok(item);         
+
+});
+
+app.MapGet("/usuario", () => cadastro.Itens.Select(Usuario => new
+
+{
+    Usuario.Id,
+    Usuario.DataNascimento,
+    Usuario.Nome,
+    EmprestimoEmAberto = Usuario.ItensEmprestados
+
+}));
+
 app.Run();
+
